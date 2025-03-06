791b44a743fece470c61feb820927972655ea1fb
kimyohun <yohunkim0@gmail.com>
2025-03-06 10:00:03 +0900

    첫 메인페이지 시험 가동

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const http = require('http');
 
 const server = http.createServer(function(request, response){
-
+    //* GET 요청 시 실행
+    if(request.method === 'GET'){
+        if(request.url === '/'){
+            console.log("첫 메인페이지");
+        }
+    }
 })
+
+
+
+
+
 
 const PORT = 3000;
 
+
+// * PORT : 3000 실행
 server.listen(PORT, function(){
     console.log("http://localhost:3000");
 })
