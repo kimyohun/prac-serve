35862a77d798e3f2de9677444271b82475b8704a
kimyohun <yohunkim0@gmail.com>
2025-03-06 09:57:28 +0900

    서버 시작

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("app.js 실행중");
+const http = require('http');
+
+const server = http.createServer(function(request, response){
+
+})
+
+const PORT = 3000;
+
+server.listen(PORT, function(){
+    console.log("http://localhost:3000");
+})
